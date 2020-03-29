c309c15a9501cdf20182d7ba59a267c572af81cb
mikebellami <mikebellami7@gmail.com>
2020-03-29 01:02:01 +0100

    completed live

--- live.php
+++ live.php
@@ -117,6 +117,32 @@
         padding: 40px 0px !important;
     }
 
+    .patra-mixlr{
+        position: fixed;
+        margin: 0 20px 20px 20px ;
+        padding: 10px;
+        border: 1px solid #0d8bcd;
+        border-radius: 100%;
+        width: 60px;
+        height: 60px;
+        z-index: 1000;
+        left:20px;
+        right: 0;
+        bottom: 0;
+        top:auto;
+        
+        background: #0d8bcd;
+        box-shadow: 0 0 10px rgba(0,0,0,.3);
+    }
+
+    .patra-mixlr i{
+        display: block;
+       font-size: 2.7rem;
+       margin: 1px auto 1px auto;
+       font-weight:bold;
+       color: #fff;
+    }
+
     @media only screen and (max-width: 767px) {
         .project-page__inner {
             max-width: 100vw !important;
@@ -230,8 +256,9 @@
     <main class="main-root">
         <div id="dsn-scrollbar">
 
+       
 
-        <header>
+            <header>
                 <div class="headefr-fexid headefr-fexid-onepage" data-dsn-header="project" >
                     <div class="bg has-top-bottom" id="dsn-hero-parallax-img" data-dsn-ajax="img">
                         <!-- <div class=" " data-dsn="video" data-overlay="4">
@@ -290,6 +317,7 @@
                         </div>
                     </div>
                 </div>
+                
             </header>
 
 
@@ -347,6 +375,15 @@
                     </div>
                 </div>
             </footer> -->
+
+            <!-- audio streaming  -->
+            <div class="patra-mixlr">
+                <a href="#" >
+                <i class="fas fa-podcast"></i>
+                </a>
+            </div>
+            <!-- /audio streaming  -->
+
 
             <!-- Chatra {literal} -->
             <script>
