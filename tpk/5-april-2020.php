fbdf6ecde8d3aa6ba5c3f14dc6081ab66a33e024
David Adegoke <davidadegoke31@gmail.com>
2020-04-04 21:26:49 +0100

    fix new videos grid

--- tpk/5-april-2020.php
+++ tpk/5-april-2020.php
@@ -128,7 +128,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-6 kids-week">
+                            <div class="col-sm-12 col-lg-6 col-md-6 kids-week">
                                 <div class="kcard" style="background-image: url('https://img.youtube.com/vi/P5hS0AD1M78/0.jpg'); background-position: center center;">
                                     <div class="content">
                                         <h2 class="ktitle" style="text-align: left;">
@@ -139,7 +139,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-6 kids-week">
+                            <div class="col-sm-12 col-lg-6 col-md-6 kids-week">
                                 <div class="kcard" style="background-image: url('https://img.youtube.com/vi/m2RuRFxJOU0/0.jpg'); background-position: center center;">
                                     <div class="content">
                                         <h2 class="ktitle" style="text-align: left;">
