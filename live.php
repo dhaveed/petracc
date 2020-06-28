65e9fb1a09946d4e1142d65beaf06e5053f90bef
dhaveed <davidadegoke31@yahoo.com>
2020-06-28 09:02:30 +0100

    Update live.php

--- live.php
+++ live.php
@@ -210,7 +210,7 @@
                             <div class="row justify-content-center align-items-center h-1">
                                 <div class="project-title" id="dsn-hero-parallax-title">
                                     <div class="embed-responsive embed-responsive-16by9">
-                                        <iframe  class="embed-responsive-item" src="https://www.youtube.com/embed/FA1sqW1WEqU?autoplay=1&enablejsapi=1&disablekb=1&controls=0&showinfo=0&loop=1&modestbranding=1" id="video" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>></iframe>
+                                        <iframe  class="embed-responsive-item" src="https://www.youtube.com/embed/ZCXe5xc2QIE?autoplay=1&enablejsapi=1&disablekb=1&controls=0&showinfo=0&loop=1&modestbranding=1" id="video" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>></iframe>
                                     </div>                                
                                 </div>                                
                             </div>
@@ -220,10 +220,10 @@
                                     <div class="row  mt-30">
                                         <div class="col-lg-5">
                                             <div class="pro-text ">
-                                                <h4  style="color: #fff;">Don't Curse The Darkness</h4>
+                                                <h4  style="color: #fff;">Have Faith in God's Love for You</h4>
                                                 <br>
-                                                <h5 class="text-muted" ><!--Ministering:--><strong> Pastor Adeola Ajani </strong></h5>
-                                                <p class="mt-5 text-muted">31st May, 2020</p>
+                                                <h5 class="text-muted" ><!--Ministering:--><strong> Pastor Joseph Zimughan </strong></h5>
+                                                <p class="mt-5 text-muted">28th June, 2020</p>
                                             </div>
                                         </div>
 
